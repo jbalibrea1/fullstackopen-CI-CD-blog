a7691458c885e60f17b15fd7c79be679e7dde028
Jorge Balibrea <jorge.balibrea1@gmail.com>
2022-09-06 04:32:49 +0200

    delete def port

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-let PORT = process.env.PORT || 3003
+let PORT = process.env.PORT
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
